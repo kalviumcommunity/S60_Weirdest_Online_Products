dd39f01dc02630c3ab948787e80fe4d45a96604d
V.Sahanashre <sahanashre.v@kalvium.community>
2024-03-13 15:17:33 +0530

    Added Database

--- Server.js
+++ Server.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
 app.get("/ping",(req,res) =>{
     res.send("pong")
@@ -8,3 +11,15 @@ app.get("/ping",(req,res) =>{
 app.listen(3000,()=>{
     console.log("This is local host 3000")
 })
+
+const Database = async ()  => {
+    try{
+        await mongoose.connect(process.env.Database)
+        console.log("Connected to Database")
+    }
+    catch (error){
+        console.log(error.message)
+    }
+}
+
+Database()
