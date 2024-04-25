999f67b0b4662ef199e7b2ed99d53b39bef51492
V.Sahanashre <sahanashre.v@kalvium.community>
2024-04-25 09:41:08 +0530

    Added ui and updated the user entities functionality

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose")
-const Data = require("./Data")
+const mongoose = require("mongoose");
+const Data = require("./Data");
 
-function app(){
-mongoose.connect("mongodb+srv://sahanashrev:Sahusasdi23@cluster0.agvrvhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
-    console.log("It is connected")
-})
-.catch((error)=>{
-    console.log("Error is",error)
-})
+function app() {
+  mongoose
+    .connect(
+      "mongodb+srv://sahanashrev:Sahusasdi23@cluster0.agvrvhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => {
+      console.log("It is connected");
+    })
+    .catch((error) => {
+      console.log("Error is", error);
+    });
 }
 
 const mongooseSchema = mongoose.Schema({
@@ -18,18 +21,17 @@ const mongooseSchema = mongoose.Schema({
     Price: String,
     Image_Link: String,
     Product_Details: String,
-    Created_By: String
-})
-
-const model = mongoose.model("user",mongooseSchema)
+    Created_By: { type: mongoose.Types.ObjectId, ref: "product" },
+});
+const model = mongoose.model("product", mongooseSchema);
 
 const userSchema = mongoose.Schema({
-    username :  String,
-    email : String,
-    password : String
-})
+  username: String,
+  email: String,
+  password: String,
+});
 
-const userModel = mongoose.model("userlogin",userSchema)
+const userModel = mongoose.model("userlogin", userSchema);
 // model.insertMany(Data)
 
-module.exports = {model:model, connection:app, userModel:userModel}
+module.exports = { model: model, connection: app, userModel: userModel };
--- Route.js
+++ Route.js
@@ -1,143 +1,165 @@
-const express = require("express")
-const app = express()
-const {model, userModel} = require("./App")
-const joiSchema = require("./joiSchema")
-const jwt = require("jsonwebtoken")
-const env = require("dotenv").config()
+const express = require("express");
+const app = express();
+const { model, userModel } = require("./App");
+const joiSchema = require("./joiSchema");
+const jwt = require("jsonwebtoken");
+const env = require("dotenv").config();
 
 //AuthenticateToken acts as a middleware
-const AuthenticateToken = (req,res,next) => {
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
-    console.log(token)
-    if(!token){
-        return res.sendStatus(401)
-    }
+const AuthenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.sendStatus(401);
+  }
 
-    jwt.verify(token,process.env.ACCESS_KEY,(err,user)=>{
-        if(err){
-            return res.sendStatus(403)
+  jwt.verify(token, process.env.ACCESS_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    req.id = user.id;
+    next();
+  });
+};
+
+app.get("/get", AuthenticateToken, (req, res) => {
+  const { id } = req.query;
+  console.log(id);
+  if (id) {
+    model
+      .find({ Created_By: id })
+      .then((ele) => {
+        console.log("Get route PRinting", ele);
+        res.json({ ele });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ err });
+      });
+  } else {
+    model
+      .find({})
+      .then((ele) => {
+        res.json({ ele });
+      })
+      .catch((err) => {
+        res.json({ err });
+      });
+  }
+});
+
+app.post("/post", AuthenticateToken, (req, res) => {
+  const {
+    Product,
+    Customer_Ratings,
+    Number_of_buyers_last_month,
+    Price,
+    Product_Details,
+    Image_Link,
+  } = req.body;
+  
+  const { error, value } = joiSchema.validate(req.body);
+  if (error) {
+    return res.send({
+      message: "Invalid inputs entered",
+      error: error.message,
+    });
+  }
+  
+  model
+    .create({
+      Product,
+      Customer_Ratings,
+      Number_of_buyers_last_month,
+      Price,
+      Product_Details,
+      Image_Link,
+      Created_By: req.id,
+    })
+    .then((ele) => {
+      return res.send(ele);
+    })
+    .catch((err) => {
+      return res.send(err.message);
+    });
+});
+
+app.put("/put/:key", (req, res) => {
+  const key = req.params.key;
+  model
+    .findByIdAndUpdate(key, {
+      Product: req.body.Product,
+      Customer_Ratings: req.body.Customer_Ratings,
+      Number_of_buyers_last_month: req.body.Number_of_buyers_last_month,
+      Price: req.body.Price,
+      Image_Link: req.body.Image_Link,
+      Product_Details: req.body.Product_Details,
+    })
+    .then(() => {
+      res.send("done");
+    });
+});
+
+app.delete("/delete/:key", (req, res) => {
+  const key = req.params.key;
+  model
+    .findByIdAndDelete(key)
+    .then((ele) => res.json(ele))
+    .catch((err) => res.json(err));
+});
+
+app.post("/signup", (req, res) => {
+  const { email } = req.body;
+  userModel
+    .findOne({ email })
+    .then((user) => {
+      if (user) {
+        if (user.email === email) {
+          res.json({ message: "User with this email already exist" });
         }
-        req.user = user
-        next()
+      } else {
+        userModel.create(req.body);
+      }
     })
-}
-
-app.get("/get",AuthenticateToken,(req,res)=>{
-    const {id} = req.query
-    console.log(id)
-    if(id){
-        model.find({Created_By:id})
-        .then((ele)=>{
-            console.log(ele)
-            res.json({ele})
-        })
-        .catch((err)=>{
-            console.log(err)
-            res.json({err})
-        })
-    }
-    else{
-        model.find({})
-        .then((ele)=>{
-            res.json({ele})
-        })
-        .catch((err)=>{
-            res.json({err})
-        })
-    }
-})
-
-app.post("/post",(req,res)=>{
-    const {error,value} = joiSchema.validate(req.body)
-    if(error){
-        res.json({message:"Invalid inputs entered",error:error.message})
-    }
-    else {
-        res.json({ message: "Valid inputs", data: value });
-    }
-
-    model.create(req.body)
-    .then((ele)=>{
-        res.json(ele)
-    })
-    .catch((err)=>{
-        res.json(err)
-    })
-})
-
-app.put("/put/:key",(req,res)=>{
-    const key = req.params.key;
-    model.findByIdAndUpdate(key,{
-    Product: req.body.Product,
-    Customer_Ratings: req.body.Customer_Ratings,
-    Number_of_buyers_last_month: req.body.Number_of_buyers_last_month,
-    Price: req.body.Price,
-    Image_Link: req.body.Image_Link,
-    Product_Details: req .body.Product_Details
-    }).then(()=>{res.send("done")})
-})
-
-app.delete("/delete/:key",(req,res)=>{
-    const key = req.params.key;
-    model.findByIdAndDelete(key)
-    .then(ele => res.json(ele))
-    .catch(err => res.json(err))
-})
-
-app.post("/signup",(req,res)=>{
-    const {email} = req.body
-    userModel.findOne({email})
-    .then(user=>{
-        if(user){
-            if(user.email === email){
-                res.json({message:"User with this email already exist"})
-            }
-        }
-        else{
-            userModel.create(req.body)
-        }
-    })
-    .then(user => res.json(user))
-    .catch(err => res.json({message: err}))
-})
+    .then((user) => res.json(user))
+    .catch((err) => res.json({ message: err }));
+});
 
 // Get email and password from user
-// Check database 
+// Check database
 // Return appropriate message
 
-app.post("/login", (req, res) => {
-    const {username, password} = req.body
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    userModel.findOne({username})
-    .then(user => {
-        if(user){
-            if(user.password === password){
-                // res.status(200).json({message: "Login successful"})
-                const user = {name : username}
-                const accessToken =jwt.sign(user,process.env.ACCESS_KEY)
-                res.json({accessToken})
-            }
-            else{
-                res.status(400).json({message: "Details given by the user did not match"})
-                
-            }
-        }
-        else{
-            res.status(400).json({message: "User doesn't exist. Kindly register"})
-        }
+  let data = await userModel.findOne({ username });
+
+  if (data) {
+    if (data.password === password) {
+      // res.status(200).json({message: "Login successful"})
+      const user = { name: username, id: data._id };
+      const accessToken = jwt.sign(user, process.env.ACCESS_KEY);
+      res.json({ accessToken });
+    } else {
+      res
+        .status(400)
+        .json({ message: "Details given by the user did not match" });
+    }
+  } else {
+    res.status(400).json({ message: "User doesn't exist. Kindly register" });
+  }
+});
+
+app.get("/user", AuthenticateToken, (req, res) => {
+  console.log("getuser");
+  userModel
+    .find({})
+    .then((user) => {
+      console.log(user);
+      return res.send(user);
     })
-})
+    .catch((err) => err.message);
+});
 
-app.get("/user",AuthenticateToken,(req,res)=>{
-    console.log("getuser")
-    userModel.find({})
-    .then((user)=>{
-        console.log(user)
-        res.send(user)
-    })
-    .catch((err)=>err.message)
-
-})
-
-module.exports = app
+module.exports = app;
